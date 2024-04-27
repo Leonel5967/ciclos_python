914cf407dbeccb8f84ae679b5e9609429d268d0a
leonel5967 <leonelcastro2609@gmail.com>
2024-04-27 14:35:57 -0400

    fregue mi ciclo

--- ciclos4a.py
+++ ciclos4a.py
@@ -5,9 +5,7 @@ incre=3
 while(contador<=6):
     if(sw==0):
         print(numero)
-        sw=1
-    else:
-        print(-numero)
+flkshgkshgkjhksljbnknbmkhgiaehrgjkhskjlghkñlkmngfoijkñljbnafcpokqanñihyamiohkrb896877q32roafdiutasfkljgfv
         sw=0
     numero=numero+incre
     contador=contador+1
