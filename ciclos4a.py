a10256374d140497373f464e15355c092271dbe6
leonel5967 <leonelcastro2609@gmail.com>
2024-04-27 14:39:14 -0400

    arregle mi ciclo

--- ciclos4a.py
+++ ciclos4a.py
@@ -5,7 +5,9 @@ incre=3
 while(contador<=6):
     if(sw==0):
         print(numero)
-flkshgkshgkjhksljbnknbmkhgiaehrgjkhskjlghkñlkmngfoijkñljbnafcpokqanñihyamiohkrb896877q32roafdiutasfkljgfv
+        sw=1
+    else:
+        print(-numero)
         sw=0
     numero=numero+incre
     contador=contador+1
